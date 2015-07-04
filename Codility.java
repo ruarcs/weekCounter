35846a3778f03c4c54d9ca24ce626af4eac794e2
Ruarc Sorensen <ruarcs@gmail.com>
2015-07-04 23:11:21 +0100

    Add correct file for original solution.

--- Codility.java
+++ Codility.java
@@ -16,42 +16,88 @@ public class Codility
 	private int solution( int year, int startMonth, int endMonth, int dayOfWeek )
 	{
 		int[] numDaysInMonths = numDaysInMonthForYear( year );
+		Day currentDay = Day.valueOf( dayOfWeek );
 		int weekCount = 0;
 		boolean counting = false;
-		int firstMondayInJanuary = 7 - dayOfWeek + 1;
-		int day = firstMondayInJanuary;
 		for( int currentMonth = 0; currentMonth < 12; currentMonth++ )
 		{
-			if( !counting && ( currentMonth == startMonth ) )
+			for( int day = 0; day < numDaysInMonths[ currentMonth ]; day++ )
 			{
-				counting = true;
-			}
-			
-			for( ; day <= numDaysInMonths[ currentMonth ]; day += 7 )
-			{				
-				if( counting )
+				currentDay = Day.nextDay( currentDay );
+				
+				if( !counting )
 				{
-					weekCount++;
+					if( currentMonth == startMonth )
+					{
+						counting = true;
+					}
+				}
+				else
+				{
+					if( currentMonth == ( endMonth + 1 ) )
+					{
+						// Using this logic we've counted one week
+						// too far! Subtract one.
+						weekCount--;
+						// No point counting any further!!
+						// Return the count.
+						return weekCount;
+					}
+					else if ( currentDay == Day.Monday )
+					{
+						// If it's Monday count another week.
+						weekCount++;
+					}
 				}
 			}
-			
-			if( counting && ( currentMonth == endMonth ) )
-			{
-				// It's possible that we have (in jumping by 7)
-				// counted a month that's not fully in the end month,
-				// so if we have then subtract.
-				if( day > numDaysInMonths[ currentMonth ] )
-				{
-					weekCount--;
-				}
-				// No point counting any further!!
-				break;
-			}
-			
-			// Set day to first monday in next month.
-			day = day - numDaysInMonths[ currentMonth ] ;
+		}
+		if( counting )
+		{
+			// If we give December as the final month then we can spill
+			// over into next year. If we're still counting then make sure
+			// we take a week back off.
+			weekCount--;
 		}
 		return weekCount;
+	}
+	
+	private enum Day
+	{
+		Monday,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday,
+		Sunday,
+		INVALID;
+		
+		static Day valueOf( int day)
+		{
+			switch( day )
+			{
+				case 0: return Monday;
+				case 1: return Tuesday;
+				case 2: return Wednesday;
+				case 3: return Thursday;
+				case 4: return Friday;
+				case 5: return Saturday;
+				case 6: return Sunday;
+				default: return INVALID;
+			}
+		}
+		
+		static Day nextDay( Day currentDay )
+		{
+			if( currentDay == Day.Sunday )
+			{
+				return Day.Monday;
+			}
+			else
+			{
+				return Day.values()[ currentDay.ordinal() + 1 ];
+			}
+		}
 	}
 	
 	private int[] numDaysInMonthForYear( int year )
